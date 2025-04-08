aac927252f2ca6ec0a6ba80bd365162983525ac1
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-08 10:35:20 +0800

    feat: create main page for all the routes

---
+++ app/(main)/archive/page.tsx
@@ -0,0 +1,3 @@
+export default function ArchivePage() {
+  return <div className="flex items-center justify-start p-4"></div>;
+}
---
+++ app/(main)/trash/page.tsx
@@ -0,0 +1,3 @@
+export default function TrashPage() {
+  return <div className="flex items-center justify-start p-4"></div>;
+}

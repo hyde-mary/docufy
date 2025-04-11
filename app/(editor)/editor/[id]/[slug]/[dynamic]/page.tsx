390dc7a2e8caa3ba124317db2c06e6defcb8c81e
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-11 19:42:50 +0800

    feat: return a 404 if the user tries to access a path that is not in the href list

--- app/(editor)/editor/[id]/[slug]/[dynamic]/page.tsx
+++ app/(editor)/editor/[id]/[slug]/[dynamic]/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import { useEditorStore } from "@/stores/editor-store";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { Fragment, useEffect } from "react";
+import { useAllEditorHrefs } from "@/hooks/useAllEditorHrefs";
 
 const EditorPageDynamic = () => {
   const params = useParams<{ id: string; slug: string; dynamic: string }>();
   const { data, setParams } = useEditorStore();
+  const router = useRouter();
+  const validHrefs = useAllEditorHrefs();
+  const fullPath = `/editor/${params.id}/${params.slug}/${params.dynamic}`;
+
+  const isValidPath = validHrefs.some(({ href }) => href === fullPath);
 
   useEffect(() => {
     if (params.id && params.slug) {
       setParams(params.id, params.slug);
     }
-  }, [params, setParams]);
+
+    if (!isValidPath) {
+      router.push("/404");
+    }
+  }, [params, setParams, isValidPath, router]);
+
+  if (!isValidPath) return;
 
   return (
     <Fragment>
@@ -21,9 +33,7 @@ const EditorPageDynamic = () => {
         <p>id: {params.id}</p>
         <p>slug: {params.slug}</p>
         <p>dynamic segment: {params.dynamic}</p>
-        <p>
-          Full Path: /editor/{params.id}/{params.slug}/{params.dynamic}
-        </p>
+        <p>Full Path: {fullPath}</p>
       </div>
 
       <div className="w-64 text-white">

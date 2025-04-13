89c1cabf1e48825f32ee3eb7087edb6c990b7a5e
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-13 17:44:09 +0800

    style: add bottom padding so the project details does not feel too close to the bottom

--- app/(main)/projects/[id]/[slug]/page.tsx
+++ app/(main)/projects/[id]/[slug]/page.tsx
@@ -3,7 +3,7 @@ import ProjectDetails from "@/components/main/projects/page/project-details";
 export default function ProjectPage() {
   return (
     <div className="flex justify-center p-4 min-h-screen">
-      <div className="container pt-48">
+      <div className="container pt-48 pb-24">
         <ProjectDetails />
       </div>
     </div>

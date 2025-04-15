f71494cbb9bb7b44d8e633861aa24f31d1ae2dcd
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-15 17:46:34 +0800

    feat: connect clerk with convex auth strategy

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,21 @@
-import SignInForm from "@/components/auth/sign-in-form";
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+import { useConvexAuth } from "convex/react";
 
-const SignInPage = () => {
+import SignUpForm from "@/components/auth/sign-up-form";
+import { Loader } from "@/components/loader";
+
+import { useRouter } from "next/navigation";
+
+import Image from "next/image";
+
+const SignUpPage = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const router = useRouter();
+
+  if (isLoading) return <Loader />;
+
+  if (isAuthenticated) router.replace("/");
+
   return (
     <div className="grid grid-cols-2 h-screen">
       <div className="bg-[#18181b] text-[#f0f0f0] border-r flex flex-col p-8 px-12">
@@ -30,32 +43,13 @@ const SignInPage = () => {
       </div>
       <div className="flex flex-col items-center justify-center">
         <div className="max-w-5xl text-center space-y-4">
-          <h1 className="text-2xl font-bold">Sign in to your account</h1>
-          <p className="text-muted-foreground">
-            Enter your email and your password to sign in to your account.
-            <br />
-            Don&apos;t have an account?{" "}
-            <Link href="/sign-up" className="underline">
-              Create an account.
-            </Link>
-          </p>
           <div className="mt-8">
-            <SignInForm />
+            <SignUpForm />
           </div>
-          <p className="text-sm text-muted-foreground text-center px-8 leading-5 mt-8">
-            By continuing, you agree to our{" "}
-            <Link href="#" className="underline">
-              Terms of Service
-            </Link>{" "}
-            and{" "}
-            <Link href="#" className="underline">
-              Privacy Policy
-            </Link>
-          </p>
         </div>
       </div>
     </div>
   );
 };
 
-export default SignInPage;
+export default SignUpPage;

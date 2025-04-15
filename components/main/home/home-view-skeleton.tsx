904b0c7a4f061484027e49c21bb3556145ec1885
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-15 22:30:37 +0800

    refactor/feat: create a home view skeleton component

---
+++ components/main/home/home-view-skeleton.tsx
@@ -0,0 +1,22 @@
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+const HomeViewSkeleton = () => {
+  return (
+    <div className="flex items-center justify-start py-4 gap-8 flex-wrap">
+      <Skeleton className="w-96 h-64" />
+      <Skeleton className="w-96 h-64" />
+      <Skeleton className="w-96 h-64" />
+      <Skeleton className="w-96 h-64" />
+      <Skeleton className="w-96 h-64" />
+      <Skeleton className="w-96 h-64" />
+      <Separator />
+      <Skeleton className="w-96 h-64" />
+      <Skeleton className="w-96 h-64" />
+      <Skeleton className="w-96 h-64" />
+    </div>
+  );
+};
+
+export default HomeViewSkeleton;

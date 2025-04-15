ca52f6878cae98cf29ab20899cc13d8feff1a0cf
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-15 22:30:37 +0800

    refactor/feat: create a home view skeleton component

--- components/main/home/home-view.tsx
+++ components/main/home/home-view.tsx
@@ -1,31 +1,16 @@
 import { Doc } from "@/convex/_generated/dataModel";
 
-import { Skeleton } from "@/components/ui/skeleton";
-import { Separator } from "@/components/ui/separator";
 import { ProjectsCard } from "@/components/main/projects-card";
 
 import Link from "next/link";
+import HomeViewSkeleton from "./home-view-skeleton";
 
 interface ProjectsViewProps {
   projects: Doc<"projects">[];
 }
 
 const HomeView = ({ projects }: ProjectsViewProps) => {
-  if (!projects)
-    return (
-      <div className="flex items-center justify-start py-4 gap-8 flex-wrap">
-        <Skeleton className="w-96 h-64" />
-        <Skeleton className="w-96 h-64" />
-        <Skeleton className="w-96 h-64" />
-        <Skeleton className="w-96 h-64" />
-        <Skeleton className="w-96 h-64" />
-        <Skeleton className="w-96 h-64" />
-        <Separator />
-        <Skeleton className="w-96 h-64" />
-        <Skeleton className="w-96 h-64" />
-        <Skeleton className="w-96 h-64" />
-      </div>
-    );
+  if (!projects) return <HomeViewSkeleton />;
 
   return (
     <div className="flex flex-wrap items-start gap-6 py-4">

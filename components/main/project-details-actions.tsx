4055ab1431664ef1109bdddbf03f358ca61390ce
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-16 15:47:17 +0800

    refactor: remove redundant components and implement all functionality for the 5 project actions into 1 component

---
+++ components/main/project-details-actions.tsx
@@ -0,0 +1,211 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+import {
+  Archive,
+  ArrowRight,
+  Globe,
+  Lock,
+  RefreshCcw,
+  Trash,
+} from "lucide-react";
+
+import { useState } from "react";
+
+import Link from "next/link";
+
+interface ProjectDetailsActionsProps {
+  visibility: "Private" | "Public";
+  status: "Active" | "Inactive";
+  projectId: Id<"projects">;
+  slug: string;
+}
+
+const ProjectDetailsActions = ({
+  visibility,
+  status,
+  projectId,
+  slug,
+}: ProjectDetailsActionsProps) => {
+  const archiveProject = useMutation(api.projects_mutations.archiveProject);
+  const unarchiveProject = useMutation(api.projects_mutations.unarchiveProject);
+  const publishProject = useMutation(api.projects_mutations.publishProject);
+  const unpublishProject = useMutation(api.projects_mutations.unpublishProject);
+  const deleteProject = useMutation(api.projects_mutations.deleteProject);
+
+  const [isLoading, setIsLoading] = useState(false); // global loading state for all mutations
+
+  // archive functions
+  const onArchive = () => {
+    setIsLoading(true);
+
+    const promise = archiveProject({ projectId }).finally(() =>
+      setIsLoading(false)
+    );
+
+    toast.promise(promise, {
+      success: "Project archived successfully.",
+      error: "Project archived failed.",
+    });
+  };
+
+  const onUnarchive = () => {
+    setIsLoading(true);
+
+    const promise = unarchiveProject({ projectId }).finally(() =>
+      setIsLoading(false)
+    );
+
+    toast.promise(promise, {
+      success: "Project unarchived successfully.",
+      error: "Project unarchived failed.",
+    });
+  };
+
+  // publish functions
+  const onPublish = () => {
+    setIsLoading(true);
+
+    const promise = publishProject({ projectId }).finally(() =>
+      setIsLoading(false)
+    );
+
+    toast.promise(promise, {
+      success: "Project published successfully.",
+      error: "Project publish failed.",
+    });
+  };
+
+  const onUnpublish = () => {
+    setIsLoading(true);
+
+    const promise = unpublishProject({ projectId }).finally(() =>
+      setIsLoading(false)
+    );
+
+    toast.promise(promise, {
+      success: "Project unpublished successfully.",
+      error: "Project unpublish failed.",
+    });
+  };
+
+  // delete functions
+  const onDelete = () => {
+    setIsLoading(true);
+
+    const promise = deleteProject({ projectId }).finally(() =>
+      setIsLoading(false)
+    );
+
+    toast.promise(promise, {
+      success: "Project deleted successfully.",
+      error: "Project delete failed.",
+    });
+  };
+
+  return (
+    <>
+      {visibility !== "Public" && status === "Active" && (
+        <div className="flex items-center gap-x-4">
+          <Button
+            className="hover:cursor-pointer"
+            variant={"secondary"}
+            onClick={onArchive}
+            disabled={isLoading}
+          >
+            <Archive className="w-4 h-4" />
+            Archive Project
+          </Button>
+
+          <Button
+            className="hover:cursor-pointer"
+            variant={"default"}
+            onClick={onPublish}
+            disabled={isLoading}
+          >
+            <Globe className="w-4 h-4" />
+            Publish Project
+          </Button>
+
+          <Link href={`/editor/${projectId}/${slug}`}>
+            <Button variant="outline" className="hover:cursor-pointer">
+              Go to editor
+              <ArrowRight className="w-4 h-4" />
+            </Button>
+          </Link>
+        </div>
+      )}
+
+      {visibility !== "Public" && status === "Inactive" && (
+        <div className="flex items-center justify-center gap-x-4">
+          <Button
+            className="hover:cursor-pointer"
+            variant={"default"}
+            onClick={onUnarchive}
+            disabled={isLoading}
+          >
+            <RefreshCcw className="w-4 h-4" />
+            Unarchive Project
+          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="hover:cursor-pointer" variant={"destructive"}>
+                <Trash className="w-4 h-4" />
+                Delete Project
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogTitle>Delete Project?</DialogTitle>
+              <DialogDescription>
+                Are you really sure you want to delete the project? This will
+                delete this project in our servers.{" "}
+                <span className="text-red-500">This cannot be undone</span>
+              </DialogDescription>
+              <DialogFooter>
+                <Button
+                  className="hover:cursor-pointer"
+                  variant={"destructive"}
+                  onClick={onDelete}
+                  disabled={isLoading}
+                >
+                  <Trash />
+                  Delete Project
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
+      )}
+
+      {visibility === "Public" && (
+        <div className="flex items-center justify-center gap-x-4">
+          <Button
+            className="hover:cursor-pointer"
+            variant={"secondary"}
+            onClick={onUnpublish}
+            disabled={isLoading}
+          >
+            <Lock className="w-4 h-4" />
+            Unpublish Project
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ProjectDetailsActions;
--- components/main/project-details.tsx
+++ components/main/project-details.tsx
@@ -1,39 +1,18 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { getLucideIcon } from "@/utils/components/getLucideIcon";
-import { useMutation, useQuery } from "convex/react";
-import {
-  Archive,
-  ArrowRight,
-  Globe,
-  Lock,
-  MoreHorizontal,
-  Pen,
-  RefreshCcw,
-  Trash,
-} from "lucide-react";
-import Link from "next/link";
-import { useParams, useRouter } from "next/navigation";
+import { useQuery } from "convex/react";
+
+import { useParams } from "next/navigation";
 import ProjectDetailsContent from "./project-details-content";
-import { useState } from "react";
-import { toast } from "sonner";
-import { cn } from "@/lib/utils";
 import { reorderProjectData } from "@/utils/reorder-project-data";
 
 import ProjectDetailsInformation from "@/components/main/project-details-information";
+import ProjectDetailsActions from "./project-details-actions";
 
 const ProjectDetails = () => {
   const { id } = useParams();
@@ -41,13 +20,6 @@ const ProjectDetails = () => {
   const project = useQuery(api.projects_queries.getProjectById, {
     id: id as Id<"projects">,
   });
-
-  const moveProjectToTrash = useMutation(api.projects.moveProjectToTrash);
-  const publishProject = useMutation(api.projects.publishProject);
-
-  const [isTrashing, setIsTrashing] = useState(false);
-  const [isPublishing, setIsPublishing] = useState(false);
-  const router = useRouter();
 
   if (!project) {
     return (
@@ -61,36 +33,6 @@ const ProjectDetails = () => {
 
   const data = reorderProjectData(project.data);
 
-  const handleProjectToTrash = (projectId: Id<"projects">) => {
-    setIsTrashing(true);
-    try {
-      moveProjectToTrash({ projectId });
-      toast.success("Successfully moved the project to trash");
-      router.push("/");
-    } catch (error) {
-      toast.error("Error moving project to trash", {
-        description: error as string,
-      });
-    } finally {
-      setIsTrashing(false);
-    }
-  };
-
-  const handlePublishProject = (projectId: Id<"projects">) => {
-    setIsPublishing(true);
-    try {
-      publishProject({ projectId });
-      toast.success("Successfully published the project");
-      router.push("/publish");
-    } catch (error) {
-      toast.error("Error publishing your project", {
-        description: error as string,
-      });
-    } finally {
-      setIsPublishing(true);
-    }
-  };
-
   return (
     <div className="flex flex-col gap-y-6">
       {/* Header Section */}
@@ -99,87 +41,13 @@ const ProjectDetails = () => {
           {getLucideIcon(project.iconName, 24)}
           <h1 className="text-3xl font-semibold">{project.title}</h1>
         </div>
-        {project.visibility !== "Public" && project.status === "Active" && (
-          <div className="flex items-center gap-x-4">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="hover:cursor-pointer">
-                  <MoreHorizontal className="w-4 h-4" />
-                </Button>
-              </DropdownMenuTrigger>
 
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>Project Actions</DropdownMenuLabel>
-
-                <DropdownMenuSeparator />
-
-                <DropdownMenuItem
-                  className="gap-x-4 hover:cursor-pointer"
-                  onClick={() =>
-                    router.push(`/projects/edit/${project._id}/${project.slug}`)
-                  }
-                >
-                  <Pen className="w-4 h-4" />
-                  Edit Project
-                </DropdownMenuItem>
-
-                <DropdownMenuItem
-                  className={cn(
-                    "gap-x-4 hover:cursor-pointer",
-                    isTrashing && "bg-gray-500"
-                  )}
-                  onClick={() => handleProjectToTrash(project._id)}
-                  disabled={isTrashing || isPublishing}
-                >
-                  <Archive className="w-4 h-4" />
-                  {isTrashing || isPublishing
-                    ? "Archive Project"
-                    : "Archive Project"}
-                </DropdownMenuItem>
-
-                <DropdownMenuItem
-                  className="gap-x-4 hover:cursor-pointer"
-                  onClick={() => handlePublishProject(project._id)}
-                  disabled={isTrashing || isPublishing}
-                >
-                  <Globe className="w-4 h-4" />
-                  {isTrashing || isPublishing
-                    ? "Publishing Project"
-                    : "Publish Project"}
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-
-            <Link href={`/editor/${project._id}/${project.slug}`}>
-              <Button variant="outline" className="hover:cursor-pointer">
-                Go to editor
-                <ArrowRight className="w-2 h-2" />
-              </Button>
-            </Link>
-          </div>
-        )}
-
-        {project.visibility !== "Public" && project.status === "Inactive" && (
-          <div className="flex items-center justify-center gap-x-4">
-            <Button className="hover:cursor-pointer" variant={"default"}>
-              <RefreshCcw className="w-4 h-4" />
-              Restore Project
-            </Button>
-            <Button className="hover:cursor-pointer" variant={"destructive"}>
-              <Trash className="w-4 h-4" />
-              Delete Project
-            </Button>
-          </div>
-        )}
-
-        {project.visibility === "Public" && (
-          <div className="flex items-center justify-center gap-x-4">
-            <Button className="hover:cursor-pointer" variant={"secondary"}>
-              <Lock className="w-4 h-4" />
-              Unpublish Project
-            </Button>
-          </div>
-        )}
+        <ProjectDetailsActions
+          visibility={project.visibility}
+          status={project.status}
+          projectId={project._id}
+          slug={project.slug}
+        />
       </div>
 
       <Separator />

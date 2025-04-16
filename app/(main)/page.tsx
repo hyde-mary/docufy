66200ebab7460309fd1bb14521a16787975f7231
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-16 17:17:05 +0800

    fix: broken render of main view

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,20 +1,50 @@
 "use client";
 import MainView from "@/components/main-view";
 import MainViewHeader from "@/components/main-view-header";
+import { Separator } from "@/components/ui/separator";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 
 export default function Home() {
   const projects = useQuery(api.projects_queries.getActiveProjects);
 
+  const privateProjects = projects?.filter(
+    (project) => project.visibility === "Private"
+  );
+  const publicProjects = projects?.filter(
+    (project) => project.visibility === "Public"
+  );
+
+  const isEmpty = (projects?.length ?? 0) === 0;
+
   return (
     <div className="flex flex-col items-start justify-start p-6 space-y-4 h-full">
       <MainViewHeader viewType="project" />
       <div className="w-full py-4 space-y-4 h-full flex flex-col">
-        <h1 className="text-xl font-bold">Projects</h1>
+        <h1 className="text-xl font-bold">Active Projects</h1>
+
         <div className="flex flex-1">
-          <MainView projects={projects!} />
+          <MainView
+            projects={privateProjects!}
+            isEmpty={isEmpty}
+            showCreateCard={true}
+          />
         </div>
+
+        {publicProjects && publicProjects.length > 0 && (
+          <>
+            <Separator />
+
+            <div className="w-full py-4 space-y-4">
+              <h1 className="text-xl font-bold">Public Projects</h1>
+              <MainView
+                projects={publicProjects!}
+                isEmpty={isEmpty}
+                showCreateCard={false}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- app/(main)/published/page.tsx
+++ app/(main)/published/page.tsx
@@ -7,13 +7,19 @@ import { useQuery } from "convex/react";
 export default function PublishedPage() {
   const projects = useQuery(api.projects_queries.getPublishedProjects);
 
+  const isEmpty = (projects?.length ?? 0) === 0;
+
   return (
     <div className="flex flex-col items-start justify-start p-6 space-y-4 h-full">
       <MainViewHeader viewType="project" />
       <div className="w-full py-4 space-y-4 h-full flex flex-col">
         <h1 className="text-xl font-bold">Published Projects</h1>
         <div className="flex flex-1">
-          <MainView projects={projects!} />
+          <MainView
+            projects={projects!}
+            showCreateCard={true}
+            isEmpty={isEmpty}
+          />
         </div>
       </div>
     </div>

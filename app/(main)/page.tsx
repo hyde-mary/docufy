a887027124b30e3565aa62b0ababfd636383d943
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-16 19:50:41 +0800

    refactor: change public to publish in display

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             <Separator />
 
             <div className="w-full py-4 space-y-4">
-              <h1 className="text-xl font-bold">Public Projects</h1>
+              <h1 className="text-xl font-bold">Published Projects</h1>
               <MainView
                 projects={filteredPublicProjects!}
                 isEmpty={isEmpty}

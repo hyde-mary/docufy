e5a39e512c397c973bf4285bb4a3338e380803b0
hyde-mary <hydemaryalzbayne@gmail.com>
2025-04-17 15:31:09 +0800

    fix: remove skeleton which causes an infinite loading state effect. We don't need this as there will always be data from the store.

--- components/editor/page/editor-page-header.tsx
+++ components/editor/page/editor-page-header.tsx
@@ -2,7 +2,6 @@
 
 import { ThemeToggleClick } from "@/components/theme-toggle-click";
 import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
 import { useEditorStore } from "@/stores/editor-store";
 import { Facebook, Github, Twitter } from "lucide-react";
 import Link from "next/link";
@@ -22,58 +21,37 @@ const EditorPageHeader = () => {
       <div className="flex items-center justify-center gap-x-4">
         <h1 className="text-base font-bold">
           <Link href={`/editor/${data.params.id}/${data.params.slug}`}>
-            {data.title?.trim() ? (
-              data.title
-            ) : (
-              <Skeleton className="h-8 w-24" />
-            )}
+            {data.title}
           </Link>
         </h1>
 
-        {data.navLinks.length > 0 ? (
-          data.navLinks.map((link, index) => (
-            <Link
-              key={index}
-              href={link.href}
-              className="text-sm text-muted-foreground hover:text-gray-300"
-            >
-              {link.name}
-            </Link>
-          ))
-        ) : (
-          <>
-            <Skeleton className="h-5 w-12" />
-            <Skeleton className="h-5 w-12" />
-            <Skeleton className="h-5 w-12" />
-            <Skeleton className="h-5 w-12" />
-          </>
-        )}
+        {data.navLinks.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className="text-sm text-muted-foreground hover:text-gray-300"
+          >
+            {link.name}
+          </Link>
+        ))}
       </div>
 
       <div className="flex items-center justify-center gap-x-4">
-        {data.socials.some((social) => social.href.trim() !== "") ? (
-          data.socials
-            .filter((social) => social.href.trim() !== "")
-            .map((social) => (
-              <a
-                key={social.platform}
-                href={social.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-2 text-sm"
-              >
-                <Button variant={"ghost"} className="hover:cursor-pointer">
-                  {socialsIcons[social.platform]}
-                </Button>
-              </a>
-            ))
-        ) : (
-          <>
-            <Skeleton className="h-8 w-8" />
-            <Skeleton className="h-8 w-8" />
-            <Skeleton className="h-8 w-8" />
-          </>
-        )}
+        {data.socials
+          .filter((social) => social.href.trim() !== "")
+          .map((social) => (
+            <a
+              key={social.platform}
+              href={social.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2 text-sm"
+            >
+              <Button variant={"ghost"} className="hover:cursor-pointer">
+                {socialsIcons[social.platform]}
+              </Button>
+            </a>
+          ))}
 
         {data.theme_toggle && <ThemeToggleClick />}
       </div>
--- components/editor/page/editor-page-sidebar.tsx
+++ components/editor/page/editor-page-sidebar.tsx
@@ -4,7 +4,6 @@ import { useEditorStore } from "@/stores/editor-store";
 import { ChevronDown, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { Fragment, useEffect, useState } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
 
 const EditorPageSidebar = () => {
   const { data } = useEditorStore();
@@ -24,89 +23,75 @@ const EditorPageSidebar = () => {
     setOpenDropdowns(allDropdownIndices);
   }, [data]);
 
-  const isEmpty = data.sections.length === 0;
-
   return (
     <Fragment>
-      {isEmpty ? (
-        <div className="px-4 py-2 space-y-3">
-          <Skeleton className="h-6 w-3/4" />
-          <Skeleton className="h-6 w-5/6" />
-          <Skeleton className="h-6 w-2/3" />
-          <Skeleton className="h-6 w-2/3" />
-          <Skeleton className="h-6 w-2/3" />
-          <Skeleton className="h-6 w-2/3" />
-          <Skeleton className="h-6 w-2/3" />
-        </div>
-      ) : (
-        data.sections.map((section, index) => {
-          if (section.type === "text") {
-            return (
-              <p
-                key={index}
-                className="text-base font-bold truncate w-full px-4 py-1"
-                title={section.name}
-              >
+      {data.sections.map((section, index) => {
+        if (section.type === "text") {
+          return (
+            <p
+              key={index}
+              className="text-base font-bold truncate w-full px-4 py-1"
+              title={section.name}
+            >
+              {section.name}
+            </p>
+          );
+        }
+
+        if (section.type === "link") {
+          return (
+            <Link
+              key={index}
+              href={section.href}
+              className="w-full text-base block"
+            >
+              <p className="hover:bg-muted-foreground/10 text-muted-foreground rounded-sm px-4 py-1">
                 {section.name}
               </p>
-            );
-          }
+            </Link>
+          );
+        }
 
-          if (section.type === "link") {
+        if (section.type === "dropdown") {
+          const isOpen = openDropdowns.includes(index);
+
+          if (section.name !== "") {
             return (
-              <Link
-                key={index}
-                href={section.href}
-                className="w-full text-base block"
-              >
-                <p className="hover:bg-muted-foreground/10 text-muted-foreground rounded-sm px-4 py-1">
-                  {section.name}
-                </p>
-              </Link>
+              <div key={index} className="px-4 space-y-2 py-1 w-full">
+                <button
+                  onClick={() => toggleDropdown(index)}
+                  className="flex items-center justify-between w-full text-base font-medium hover:underline hover:cursor-pointer"
+                >
+                  <p>{section.name}</p>
+                  {isOpen ? (
+                    <ChevronDown className="w-4 h-4" />
+                  ) : (
+                    <ChevronRight className="w-4 h-4" />
+                  )}
+                </button>
+
+                {isOpen && (
+                  <div className="space-y-2">
+                    {section.items.map((item, itemIndex) => (
+                      <Link
+                        key={itemIndex}
+                        href={item.href}
+                        className="w-full text-base block"
+                      >
+                        <p className="hover:bg-muted-foreground/10 text-muted-foreground rounded-sm px-4 py-1">
+                          {item.name}
+                        </p>
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
             );
           }
+        }
 
-          if (section.type === "dropdown") {
-            const isOpen = openDropdowns.includes(index);
-
-            if (section.name !== "") {
-              return (
-                <div key={index} className="px-4 space-y-2 py-1 w-full">
-                  <button
-                    onClick={() => toggleDropdown(index)}
-                    className="flex items-center justify-between w-full text-base font-medium hover:underline hover:cursor-pointer"
-                  >
-                    <p>{section.name}</p>
-                    {isOpen ? (
-                      <ChevronDown className="w-4 h-4" />
-                    ) : (
-                      <ChevronRight className="w-4 h-4" />
-                    )}
-                  </button>
-
-                  {isOpen && (
-                    <div className="space-y-2">
-                      {section.items.map((item, itemIndex) => (
-                        <Link
-                          key={itemIndex}
-                          href={item.href}
-                          className="w-full text-base block"
-                        >
-                          <p className="hover:bg-muted-foreground/10 text-muted-foreground rounded-sm px-4 py-1">
-                            {item.name}
-                          </p>
-                        </Link>
-                      ))}
-                    </div>
-                  )}
-                </div>
-              );
-            }
-          }
-
-          return null;
-        })
-      )}
+        return null;
+      })}
     </Fragment>
   );
 };
